6475a5aa73e74988be8e16e12105d038fab18fde
Matt Kulka <92747257+MattKulka@users.noreply.github.com>
2024-05-06 13:37:34 -0400

    Touch up settings

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '4cdc30be-362c-4f9e-8643-1999b55f0c26';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '4cdc30be-362c-4f9e-8643-1999b55f0c26';
 
 const Modal = () => {
   const [username, setUsername] = useState('');
